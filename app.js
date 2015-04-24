5f79cf269e9ba3f01191dc2d4c346fab6b09cc99
Slash <y12341234@gmail.com>
2015-04-24 18:11:04 +0800

    codeship

--- app.js
+++ app.js
@@ -65,6 +65,7 @@ var server = app.listen(3000, function () {
 
     console.log('Example app listening at http://%s:%s', host, port)
 
-})
+});
+
 
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,52 @@
+var server = require('../app.js');
+
+describe('server', function () {
+  before(function () {
+    server.listen(3000);
+  });
+
+  after(function () {
+    server.close();
+  });
+});
+
+var assert = require('assert'),
+    http = require('http');
+
+describe('/', function () {
+  it('should return 200', function (done) {
+    http.get('http://localhost:3000', function (res) {
+      assert.equal(200, res.statusCode);
+      done();
+    });
+  });
+
+  it('should say "Hello, world!"', function (done) {
+    http.get('http://localhost:3000', function (res) {
+      var data = '';
+
+      res.on('data', function (chunk) {
+        data += chunk;
+      });
+
+      res.on('end', function () {
+        assert.equal('Hello, world!\n', data);
+        done();
+      });
+    });
+  });
+});
+
+
+
+
+
+
 var assert = require("assert")
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
-      assert.equal(0, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     })
   })
